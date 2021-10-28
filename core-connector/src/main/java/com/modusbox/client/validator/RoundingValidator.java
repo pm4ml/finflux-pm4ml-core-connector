0e58aea29825a0a97f4449c14ccb4741f54b4b2d
Myo Min Htet <myo.htet@thitsaworks.com>
2021-10-28 11:43:08 +0630

    MMD-1667 Update environment setup for rounding value.

--- core-connector/src/main/java/com/modusbox/client/validator/RoundingValidator.java
+++ core-connector/src/main/java/com/modusbox/client/validator/RoundingValidator.java
@@ -1,9 +1,8 @@
 package com.modusbox.client.validator;
 
-import com.modusbox.client.model.QuoteRequest;
-import com.modusbox.client.common.Constants;
 import com.modusbox.client.customexception.CCCustomException;
 import com.modusbox.client.enums.ErrorCode;
+import com.modusbox.client.model.QuoteRequest;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
@@ -11,16 +10,15 @@ public class RoundingValidator implements Processor {
 
     public void process(Exchange exchange) throws Exception {
 
+
         QuoteRequest quoteRequest = exchange.getIn().getBody(QuoteRequest.class);
         String amount = quoteRequest.getAmount();
-//        exchange.setProperty("amount", amount);
 
         float famount =  Float.parseFloat(amount);
         System.out.println("Amount in request body:"+famount);
-//        System.out.println("Constant Rounding Value:"+Constants.ROUNDING_VALUE);
-        System.out.print("Clientid : "+exchange.getProperty("clientId"));
         System.out.println("Constant Rounding Value:"+exchange.getProperty("roundingvalue"));
-        if ((famount % Constants.ROUNDING_VALUE) != 0) {
+
+        if ((famount % Short.parseShort((String) exchange.getProperty("roundingvalue"))) != 0) {
             throw new CCCustomException(ErrorCode.getErrorResponse(ErrorCode.GENERIC_ID_NOT_FOUND, "Amount is invalid. Please enter the amount in multiple of 50.(e.g. 50, 100, 150, 200, etc)"));
         }
     }
